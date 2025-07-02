3f8616ec3ed172fdc8d1a6a10f58a35f1d3060e1
Ian Simpson <53959046+INSCCOIN@users.noreply.github.com>
2025-07-02 08:28:56 -0400

    Update MegaEdit.ino

--- MegaEdit/MegaEdit.ino
+++ MegaEdit/MegaEdit.ino
@@ -1,5 +1,6 @@
 // Arduino Mega 2560 Memory Editor/Viewer
 // INSCCOIN 2025 V 1.0
+// CHATGPT COMMENTED THIS CODE, CHATGPT DID NOT WRITE THIS CODE
 // Usage via Serial Monitor:
 //   r <address> <length>   - Read memory
 //   w <address> <value>    - Write memory
